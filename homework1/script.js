4dccd6e5fa7a79f137d04615d71b973e0e738a03
Florian Ramaj <Florian Ramaj@DESKTOP-D2REE14>
2019-10-26 17:50:50 +0200

    commit

---
+++ homework1/script.js
@@ -0,0 +1,30 @@
+AOS.init();
+var clickedButton = false;
+
+function showText() {
+    alert(document.getElementById("firstName").value + " says " + document.getElementById("TextArea").value);
+}
+function showAnimation() {
+    var c = document.getElementById("idProgress").childNodes;
+    var b = 0;
+
+    if (!clickedButton) {
+        clickedButton = true;
+    }
+    else {
+        clickedButton = false;
+    }
+    
+    for (i = 0; i < c.length; i++) {
+        if (c[i].classList == "progress")
+        {
+            if (clickedButton) {
+                c[i].childNodes[1].classList.add("progress-bar-animated");
+            }
+            else {
+                c[i].childNodes[1].classList.remove("progress-bar-animated");
+            }
+        }
+    }
+}
+
